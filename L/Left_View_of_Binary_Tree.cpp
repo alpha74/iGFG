5b92b92f164057f81496aeacc3d5c67caed84fde
Aman Kumar <amanalphakumar@gmail.com>
2021-09-05 23:37:54 +0530

    Update Left_View_of_Binary_Tree.cpp

--- L/Left_View_of_Binary_Tree.cpp
+++ L/Left_View_of_Binary_Tree.cpp
@@ -3,6 +3,7 @@
 // Aman Kumar
 
 /*
+  Method1 : Using height
   - Maintain two variables: height and maxheight.
   - Push root value when height > maxheight. Update maxheight.
 */
@@ -29,4 +30,60 @@ vector<int> leftView(Node *root)
    int maxh =0 ;
    lview( root, res, 1, maxh) ;
    return res ;
+}
+
+
+/*
+  Method2: Using 2 queues
+*/
+
+vector<int> leftView(Node *root)
+{
+    queue<Node*> q1, q2 ;
+    
+    if( root != NULL)
+    q1.push( root ) ;
+    
+    vector<int> ret ;
+    
+    while( !q1.empty() || !q2.empty() )
+    {
+        if( !q1.empty() )
+        {
+            Node *curr = q1.front() ;
+            
+            ret.push_back( curr -> data );
+            
+            while( !q1.empty() )
+            {
+                Node *lev = q1.front() ;
+                
+                if( lev -> left != NULL )
+                    q2.push( lev -> left ) ;
+                if( lev -> right != NULL )
+                    q2.push( lev -> right ) ;
+               
+                 q1.pop() ;
+            }
+        }
+        else if( !q2.empty()  )
+        {
+            Node *curr = q2.front() ;
+            ret.push_back( curr -> data ) ;
+            
+            while( !q2.empty() )
+            {
+                Node *lev = q2.front() ;
+                
+                 if( lev -> left != NULL )
+                    q1.push( lev -> left ) ;
+                if( lev -> right != NULL )
+                    q1.push( lev -> right ) ;
+                
+                q2.pop() ;
+            }
+            
+        }
+    }
+    return ret ;
 }
