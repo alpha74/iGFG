85ec4d3dc2ebe99a357765b18556dfb38a1837de
Aman Kumar <amanalphakumar@gmail.com>
2021-09-12 22:50:48 +0530

    Update Non_Repeating_Character.cpp

--- N/Non_Repeating_Character.cpp
+++ N/Non_Repeating_Character.cpp
@@ -5,31 +5,23 @@
 // The code already written on GFG is wrong. The return type is char, but it expects -1 to be returned when no answer.
 
 //Function to find the first non-repeating character in a string.
-    int nonrepeatingCharacter(string S)
+char nonrepeatingCharacter(string S)
+{
+    int freq[ 28 ] = { 0 } ;
+
+    for( int i = 0 ; i < S.length() ; i++ )
     {
-       map<char,int> hash ;
-       
-       char ret = '#' ;
-       int len = S.length() ;
-       
-       for( int i = 0 ; i < len ; i++ )
-       {
-           auto iter = hash.find( S[i] ) ;
-           
-           if( iter == hash.end() )
-                hash[ S[i] ] = 1 ;
-            else
-                hash[ S[i] ]++ ;
-            
-       }
-       
-       for( int i = 0 ; i < len && ret == '#' ; i++ )
-       {
-           if( hash[ S[i] ] == 1 )
-            ret = S[i] ;
-       }
-       
-       if( ret == '#' )
-        return -1
-       return ret ;
+        freq[ S[i] -'a' ]++ ;
     }
+
+    char ret = '$' ;
+
+   for( int i = 0 ; i < S.length() && ret == '$' ; i++ )
+   {
+       if( freq[ S[i] -'a'] == 1 )
+        ret = S[i] ;
+   }
+
+   return ret ;
+
+}
